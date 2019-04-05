031e56f833f3b3542cccead27223c68b94296bfb
Mariia <mariiapopovych@gmail.com>
2019-04-04 19:03:32 -0700

    change js logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,22 @@
-$(document).ready(function(){
-  $("form#form").submit(function(event){
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
     event.preventDefault();
 
-   var user_answer = $("input#palindrome_user").val();
+    var user_answer = $("input#palindrome_user").val();
+    var res = "";
 
+    function checkPalindrom(user_answer) {
 
-   function palindrom (str) {
-     var sentence = " " ;
-     for ( var i = str.length - 1; i > 1 ; i--) {
-       sentence += str[i];
-       return sentence;
-   }
- }
-   $("#finalResult").text(palindrom(user_answer));
-   $("#hidden").show();
- });
- });
+      for (var i = user_answer.length; i > 0; i--) {
+        if (user_answer[i] === user_answer.charAt(user_answer.length - 1 - i)) {
+          res = "Is palindrom"
+        } else {
+          res = "In not palindrom"
+        }
+      }
+    }
+    checkPalindrom(user_answer)
+    console.log(res)
+
+  });
+});
